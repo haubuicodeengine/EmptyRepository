75ade572f8fa5f8058fe38f37d8957ed38735df2
thuyct99 <thuycao625@gmail.com>
2020-10-19 17:26:16 +0700

    Update the structure and config hibernate.

--- src/hibernate/dao/BookDao.java
+++ src/hibernate/dao/BookDao.java
@@ -1,5 +1,35 @@
 package hibernate.dao;
 
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import hibernate.entities.Book;
+import hibernate.util.HibernateUtil;
+
 public class BookDao {
+
+    public List < Book > getAllBook() {
+
+        Transaction transaction = null;
+        List < Book > listOfBook = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+            // get an user object
+
+            listOfBook = session.createQuery("from Book").getResultList();
+
+            // commit transaction
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+        return listOfBook;
+    }
 
 }
--- src/hibernate/entities/Book.java
+++ src/hibernate/entities/Book.java
@@ -1,5 +1,82 @@
 package hibernate.entities;
 
-public class Book {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "books")
+public class Book {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+	private int bookId;
+
+	@Column(name = "name")
+	private String bookName;
+
+	@Column(name = "price")
+	private float price;
+
+	@Column(name = "quantity")
+	private int quantity;
+
+	@Column(name = "author")
+	private String author;
+
+	public Book() {
+		super();
+	}
+
+	public Book(int bookId, String bookName, float price, int quantity, String author) {
+		super();
+		this.bookId = bookId;
+		this.bookName = bookName;
+		this.price = price;
+		this.quantity = quantity;
+		this.author = author;
+	}
+
+	public int getBookId() {
+		return bookId;
+	}
+
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+
+	public String getBookName() {
+		return bookName;
+	}
+
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
 }
