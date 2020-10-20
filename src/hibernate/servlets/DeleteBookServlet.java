c7bfd3239eaf4cd161efadd9c5c499d0f39a2d32
thuyct99 <thuycao625@gmail.com>
2020-10-20 15:56:26 +0700

    Update servlets.

---
+++ src/hibernate/servlets/DeleteBookServlet.java
@@ -0,0 +1,52 @@
+package hibernate.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import hibernate.dao.BookDao;
+
+/**
+ * Servlet implementation class DeleteBookServlet
+ */
+@WebServlet("/deleteBook")
+public class DeleteBookServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    private BookDao bookDao;
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DeleteBookServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+	public void init() {
+		bookDao = new BookDao();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		int bookId = Integer.parseInt(request.getParameter("id"));
+		bookDao.deleteBook(bookId);
+
+		response.sendRedirect(request.getContextPath() + "/books");
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/hibernate/servlets/EditBookServlet.java
@@ -0,0 +1,82 @@
+package hibernate.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Hibernate.validation.BookValidation;
+import hibernate.dao.BookDao;
+import hibernate.entities.Book;
+
+/**
+ * Servlet implementation class EditBookServlet
+ */
+@WebServlet("/editBook")
+public class EditBookServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private BookDao bookDao;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public EditBookServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public void init() {
+		bookDao = new BookDao();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		int id = Integer.parseInt(request.getParameter("id"));
+		Book bookEdit = bookDao.getBookById(id);
+		request.setAttribute("bookEdit", bookEdit);
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/view/addAndEditBook/index.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		int id = Integer.parseInt(request.getParameter("id"));
+		String name = request.getParameter("name");
+		String author = request.getParameter("author");
+
+		if (!BookValidation.checkEmpty(name)) {
+			request.setAttribute("error", "The book's name is not null.");
+
+			RequestDispatcher dispatcher = this.getServletContext()
+					.getRequestDispatcher("/WEB-INF/view/addAndEditBook/index.jsp");
+			dispatcher.forward(request, response);
+
+		} else if (!BookValidation.checkExisted(id, name, bookDao.getAllBook())) {
+			request.setAttribute("error", "The book's name is existed.");
+
+			RequestDispatcher dispatcher = this.getServletContext()
+					.getRequestDispatcher("/WEB-INF/view/addAndEditBook/index.jsp");
+			dispatcher.forward(request, response);
+
+		} else {
+			Book book = new Book(id, name, author);
+			bookDao.updateBook(book);
+			response.sendRedirect("books");
+		}
+	}
+}
