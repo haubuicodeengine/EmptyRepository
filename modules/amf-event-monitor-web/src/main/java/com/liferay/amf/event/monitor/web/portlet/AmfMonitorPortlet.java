d3ad23c3c99459f8965216c394961b907e77f29e
HuongNguyenThiThu <huong.nguyen@gs.liferay.com>
2021-05-10 15:59:26 +0700

    registration step1-setup

---
+++ modules/amf-event-monitor-web/src/main/java/com/liferay/amf/event/monitor/web/portlet/AmfMonitorPortlet.java
@@ -0,0 +1,30 @@
+package com.liferay.amf.event.monitor.web.portlet;
+
+import com.liferay.amf.event.monitor.web.constants.AmfMonitorPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author ces-user
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=amf-event-monitor",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=AmfMonitor",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + AmfMonitorPortletKeys.AMFMONITOR,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class AmfMonitorPortlet extends MVCPortlet {
+}
